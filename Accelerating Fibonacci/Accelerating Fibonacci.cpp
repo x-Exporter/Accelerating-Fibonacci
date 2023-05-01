25cb0396cc4acd32bf4e9556786068a4f348c186
kobol <kobol@EXPORTER>
2023-05-01 13:46:56 +0300

    Добавьте файлы проекта.

---
+++ Accelerating Fibonacci/Accelerating Fibonacci.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int fib_helper(int n, std::vector<int>& memo) 
+{
+    if (memo[n] != -1) 
+    {
+        return memo[n];
+    }
+    if (n <= 1) 
+    {
+        memo[n] = n;
+    }
+    else 
+    {
+        memo[n] = fib_helper(n - 1, memo) + fib_helper(n - 2, memo);
+    }
+    return memo[n];
+}
+
+int fib(int n) 
+{
+    std::vector<int> memo(n + 1, -1);
+    return fib_helper(n, memo);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int num;
+    cout << "Введите n: ";
+    cin >> num;
+    cout << fib(num);
+}
